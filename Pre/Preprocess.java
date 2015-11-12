e541ec1a4f084e40f8c024d1660bf47eac256230
Srijan Sood <srijansood@gmail.com>
2015-11-12 02:13:17 -0500

    Preprocessing init

---
+++ Pre/Preprocess.java
@@ -0,0 +1,12 @@
+/**
+ * Serves as a helper class for the
+ * DE2Bot Travelling Salesbot Problem
+ * Takes in a starting co-ordinate and 12 others
+ * and returns the optimal ordering in which the robot
+ * should visit them
+ */
+public class Preprocess {
+    public static void main(String[] args) {
+
+    }
+}
